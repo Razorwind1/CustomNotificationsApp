4f18be0abc30698c41a9d62c01d81772d91764b2
liampalmer0 <49377856+liampalmer0@users.noreply.github.com>
2021-02-17 00:33:26 -0500

    add static group list bindings

--- app/src/main/java/edu/uc/hickmadc/customnotifications/ui/main/GroupsFragment.kt
+++ app/src/main/java/edu/uc/hickmadc/customnotifications/ui/main/GroupsFragment.kt
@@ -5,14 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import edu.uc.hickmadc.customnotifications.R
+import androidx.fragment.app.viewModels
+import edu.uc.hickmadc.customnotifications.adapters.GroupsAdapter
+import edu.uc.hickmadc.customnotifications.databinding.FragmentGroupsBinding
+import edu.uc.hickmadc.customnotifications.dto.Group
+import java.util.ArrayList
 
 class GroupsFragment : Fragment() {
+
+    private val viewModel: MainViewModel by viewModels()
+
+    private var _binding: FragmentGroupsBinding? = null
+
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_groups, container, false)
+        _binding = FragmentGroupsBinding.inflate(inflater, container, false)
+        val view = binding.root
+//        context ?: return binding.root
+        val groups: ArrayList<Group> = viewModel.getAllGroups()
+        val adapter =
+            GroupsAdapter(groups)
+        binding.groupList.adapter = adapter
+        binding.fabAdd.setOnClickListener { viewModel.exampleClick() }
+
+        return view
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
