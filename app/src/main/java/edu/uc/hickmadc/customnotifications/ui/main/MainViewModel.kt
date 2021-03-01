77a58e0985bf6e371de710f62c79792d34a1e640
rosaricd <rosaricd@mail.uc.edu>
2021-03-01 12:18:53 -0500

    Updated MVM
    - basic getters and setters for preliminary variables

--- app/src/main/java/edu/uc/hickmadc/customnotifications/ui/main/MainViewModel.kt
+++ app/src/main/java/edu/uc/hickmadc/customnotifications/ui/main/MainViewModel.kt
@@ -1,7 +1,22 @@
 package edu.uc.hickmadc.customnotifications.ui.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import edu.uc.hickmadc.customnotifications.dto.Notification
+import edu.uc.hickmadc.customnotifications.service.NotificationService
+import kotlinx.coroutines.*
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    private var _notifications: MutableLiveData<ArrayList<Notification>> = MutableLiveData<ArrayList<Notification>>()
+    private var _notification = Notification()
+
+    internal var notifications: MutableLiveData<ArrayList<Notification>>
+        get() {return _notifications}
+        set(value) {_notifications = value}
+
+    internal var notification: Notification
+        get() {return _notification}
+        set(value) {_notification = value}
 }
