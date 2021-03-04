0ab7408c6112d2c32331c1d6d90a2a8f687ecaee
willi6m7 <willi6m7@mail.uc.edu>
2021-03-04 14:23:23 -0500

    Code Review 1
    Made a couple code review changes for the recent master branch.

--- app/src/main/java/edu/uc/hickmadc/customnotifications/ui/main/MainFragment.kt
+++ app/src/main/java/edu/uc/hickmadc/customnotifications/ui/main/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import edu.uc.hickmadc.customnotifications.R
 
 class MainFragment : Fragment() {
@@ -15,17 +16,28 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
+    /**
+     * Creates the view.
+     * @param inflater The layout inflater
+     * @param container the main view that contains sub-views
+     * @param savedInstanceState The current instance.
+     * @return The layout of the application's UI.
+     */
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-//    override fun onActivityCreated(savedInstanceState: Bundle?) {
-//        super.onActivityCreated(savedInstanceState)
-//        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-//        // TODO: Use the ViewModel
-//    }
+    /**
+     * Connects to the MainViewModel.
+     * @param savedInstanceState The current instance.
+     */
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    }
 
 }
