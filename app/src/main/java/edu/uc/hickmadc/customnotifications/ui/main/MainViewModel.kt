906ba4733686c63ec7c95078b824cd28359abb07
rosaricd <rosaricd@mail.uc.edu>
2021-03-11 09:03:13 -0500

    Gramanma suggestion
    - javadocs

--- app/src/main/java/edu/uc/hickmadc/customnotifications/ui/main/MainViewModel.kt
+++ app/src/main/java/edu/uc/hickmadc/customnotifications/ui/main/MainViewModel.kt
@@ -10,10 +10,17 @@ class MainViewModel : ViewModel() {
     var groups : ArrayList<Group> = ArrayList()
     var groupService : GroupService = GroupService()
 
+    /**
+     * Debugging function for add group button.
+     */
     fun exampleClick() {
         Logger.getAnonymousLogger().warning("You just pressed the add group button")
     }
 
+    /**
+     * Fetch all groups.
+     * @return an ArrayList of groups
+     */
     fun getAllGroups(): ArrayList<Group> {
         groups = groupService.getAllGroups()
         return groups
