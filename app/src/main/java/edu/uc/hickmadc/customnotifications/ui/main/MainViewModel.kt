46f84eab51112c900cce0a9e05f2889a18d0e916
rosaricd <rosaricd@mail.uc.edu>
2021-03-15 13:29:19 -0400

    Updated mvm
    - support for room DB

--- app/src/main/java/edu/uc/hickmadc/customnotifications/ui/main/MainViewModel.kt
+++ app/src/main/java/edu/uc/hickmadc/customnotifications/ui/main/MainViewModel.kt
@@ -1,7 +1,9 @@
 package edu.uc.hickmadc.customnotifications.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import edu.uc.hickmadc.customnotifications.dto.Group
+import edu.uc.hickmadc.customnotifications.dto.Notification
 import edu.uc.hickmadc.customnotifications.service.GroupService
 import java.util.logging.Logger
 
@@ -9,6 +11,8 @@ class MainViewModel : ViewModel() {
 
     var groups : ArrayList<Group> = ArrayList()
     var groupService : GroupService = GroupService()
+    private var _notification = Notification()
+    private var _notifications : MutableLiveData<ArrayList<Notification>> = MutableLiveData<ArrayList<Notification>>()
 
     /**
      * Debugging function for add group button.
@@ -24,6 +28,14 @@ class MainViewModel : ViewModel() {
     fun getAllGroups(): ArrayList<Group> {
         groups = groupService.getAllGroups()
         return groups
-
     }
+
+
+    internal var notification: Notification
+    get() {return _notification}
+    set(value) {_notification = value}
+
+    internal var notifications: MutableLiveData<ArrayList<Notification>>
+    get() {return _notifications}
+    set(value) {_notifications}
 }
