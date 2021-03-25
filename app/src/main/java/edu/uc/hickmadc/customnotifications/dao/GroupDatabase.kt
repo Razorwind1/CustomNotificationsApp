12d3044e9169754463344f0f3d2bc44780df0fa8
MSG17 <37581557+MSG17@users.noreply.github.com>
2021-03-24 22:31:17 -0400

    Created room database file for Group

---
+++ app/src/main/java/edu/uc/hickmadc/customnotifications/dao/GroupDatabase.kt
@@ -0,0 +1,10 @@
+package edu.uc.hickmadc.customnotifications.dao
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.uc.hickmadc.customnotifications.dto.Group
+
+@Database(entities=arrayOf(Group::class), version=1)
+abstract class GroupDatabase : RoomDatabase() {
+    abstract  fun GroupDAO() : iGroupDAO
+}
