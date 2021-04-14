90a9840098de01bb344bade6f9eac06099c5bdd1
liampalmer0 <49377856+liampalmer0@users.noreply.github.com>
2021-04-14 17:28:28 -0400

    add ability to schedule repeating alarms

--- app/src/main/java/edu/uc/hickmadc/customnotifications/service/AlarmService.kt
+++ app/src/main/java/edu/uc/hickmadc/customnotifications/service/AlarmService.kt
@@ -4,17 +4,15 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.SystemClock
 import edu.uc.hickmadc.customnotifications.dto.Notification
 import edu.uc.hickmadc.customnotifications.receiver.AlarmReceiver
 
 class AlarmService(private val context: Context) {
     private val alarmManager: AlarmManager? =
         context.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
-    private lateinit var alarmIntent: PendingIntent
 
-    fun setSingleAlarm(triggerMillis: Long, notification: Notification) {
-        alarmIntent = Intent(context, AlarmReceiver::class.java).let { intent ->
+    private fun buildIntent(notification: Notification): PendingIntent {
+        return Intent(context, AlarmReceiver::class.java).let { intent ->
             with(intent) {
                 putExtra("EXTRA_NOTIFICATION_TITLE", notification.title)
                 putExtra("EXTRA_NOTIFICATION_DESC", notification.desc)
@@ -22,10 +20,22 @@ class AlarmService(private val context: Context) {
             }
             PendingIntent.getBroadcast(context, 0, intent, 0)
         }
+    }
+
+    fun setSingleAlarm(triggerMillis: Long, notification: Notification) {
         alarmManager?.set(
-            AlarmManager.ELAPSED_REALTIME_WAKEUP,
-            SystemClock.elapsedRealtime() + triggerMillis,
-            alarmIntent
+            AlarmManager.RTC_WAKEUP,
+            triggerMillis,
+            buildIntent(notification)
+        )
+    }
+
+    fun setRepeatingAlarm(notification: Notification, triggerMillis: Long, intervalMillis: Long) {
+        alarmManager?.setRepeating(
+            AlarmManager.RTC_WAKEUP,
+            triggerMillis,
+            intervalMillis,
+            buildIntent(notification)
         )
     }
 }
