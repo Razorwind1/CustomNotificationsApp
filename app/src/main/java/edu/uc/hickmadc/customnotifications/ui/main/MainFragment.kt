076357fc5cd198f922c0f9f1827b493f28b36c79
meyer3cj <meyer3cj@users.noreply.github.com>
2021-04-14 20:24:03 -0400

    Changed list ui

--- app/src/main/java/edu/uc/hickmadc/customnotifications/ui/main/MainFragment.kt
+++ app/src/main/java/edu/uc/hickmadc/customnotifications/ui/main/MainFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import edu.uc.hickmadc.customnotifications.adapters.NotificationsAdapter
